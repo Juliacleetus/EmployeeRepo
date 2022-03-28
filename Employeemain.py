102b0dc59d50fb00fec081588ffe8bdcb188245f
Julia Alen <julia.alen@glorifi.com>
2022-03-28 19:42:32 +0530

    Employees commit

---
+++ Employeemain.py
@@ -0,0 +1,14 @@
+from Employeeclass import *
+
+employee1001 = Employee("1001", "Ram", "Delhi", "15000")
+employee1002 = Employee("1002", "Joshi", "Banglore", "20000")
+employee1003 = Employee("1003", "Peter","Lucknow","25000")
+employee1004 = Employee("1004", "Ashna","Chennai","25000")
+employee1005 = Employee("1005", "Raghav","Kolkata","25000")
+
+
+employee1001.getemployee()
+employee1002.getemployee()
+employee1003.getemployee()
+employee1004.getemployee()
+employee1005.getemployee()
